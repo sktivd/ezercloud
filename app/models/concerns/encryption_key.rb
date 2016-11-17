6c449897af48d9d1a23779f49ebe255cbbfedc41
Sung Gon Yi <skonmeme@gmail.com>
2016-11-17 14:08:15 +0900

    * wrong environmental variable was fixed.

--- app/models/concerns/encryption_key.rb
+++ app/models/concerns/encryption_key.rb
@@ -2,7 +2,7 @@ module EncryptionKey
   extend ActiveSupport::Concern  
   
   def encryption_key
-    raise 'ATTR_ENCRYPTED_KEY environmental variable is prerequisite!' if Rails.env.production? && ENV['TOKEN_KEY'].nil?
+    raise 'ATTR_ENCRYPTED_KEY environmental variable is prerequisite!' if Rails.env.production? && ENV['ATTR_ENCRYPTED_KEY'].nil?
     ENV['ATTR_ENCRYPTED_KEY'] || 'test_key'
   end
   
